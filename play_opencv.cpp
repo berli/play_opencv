07c12cb7edb0a51a03b2eab2aca52191e33c415d
berli <libo22@gmail.com>
2019-06-03 15:25:43 +0800

    modified:   play_opencv.cpp 	new file:   simsun.ttc

--- play_opencv.cpp
+++ play_opencv.cpp
@@ -3,6 +3,7 @@
 #include <string.h>
 //#include <opencv/cv.h>
 #include <stdio.h>
+#include <opencv2/freetype.hpp>
 
 using namespace std;
 using namespace cv;
@@ -35,20 +36,55 @@ float computeAngle(cv::Point pt0, cv::Point pt1)
 	return angle;
 }
 
-void frameText(cv::Mat&img, vector<string>&text)
+Scalar getFilledColor()
+{
+	static size_t i = 0;
+
+	vector<Scalar> vecScalar;
+
+	Scalar r = Scalar(229, 61, 25);//r
+	vecScalar.push_back(r);
+	
+	Scalar b = Scalar(91, 229, 25);//b
+	vecScalar.push_back(b);
+
+	Scalar g = Scalar(25, 193, 229);//g
+	vecScalar.push_back(g);
+
+	
+	r = Scalar(163, 25, 229);//r
+	vecScalar.push_back(r);
+	
+	r = Scalar(49, 129, 236);//r
+	vecScalar.push_back(r);
+	
+	r = Scalar(236, 49, 194);//r
+	vecScalar.push_back(r);
+
+	return vecScalar[i++%vecScalar.size()];
+}
+
+void frameText(cv::Mat&img, vector<string>&text, const bool&filled = true, const bool&transparent = true, const float&alpha = 0.3)
 {
 	//设置绘制文本的相关参数
 	//std::string text = "Hello World!";
 	int font_face = cv::FONT_HERSHEY_COMPLEX; 
 	double font_scale = 1;
-	int thickness = 2;
 	int baseline;
 	//获取文本框的长宽
 	int y = 0;
 	cv::Point origin; 
+
+	Mat dst;
+	if(transparent)
+	{
+		img.copyTo(dst);
+	}
+
 	for(auto&e:text)
 	{
-		cv::Size text_size = cv::getTextSize(e, font_face, font_scale, thickness, &baseline);
+		int text_thickness =2;
+		cv::Size text_size = cv::getTextSize(e, font_face, font_scale, text_thickness, &baseline);
 								 
 		//将文本框居中绘制
 		origin.x = 0;
@@ -57,6 +93,20 @@ void frameText(cv::Mat&img, vector<string>&text)
 	    origin.y += y;
 		y += origin.y;
 		cout<<"y:"<<y<<endl;
+ 
+		Rect select;//声明矩形
+		select.x = origin.x;//左上角坐标
+		select.y = origin.y - text_size.height - 5;
+		select.width = text_size.width;
+		select.height = text_size.height+12;
+		
+		int thickness = 3;
+    	if(filled)
+    		thickness = FILLED;
+		Scalar s = getFilledColor();
+		rectangle(img,select, s,thickness,8,0);//用矩形画矩形窗
+		//rectangle(img,select,Scalar(0,0,255),3,8,0);//用矩形画矩形窗
+		
 		/*
     	void cv::putText(  
 		 cv::Mat& img, // 待绘制的图像  
@@ -70,18 +120,92 @@ void frameText(cv::Mat&img, vector<string>&text)
 		 bool bottomLeftOrigin = false // true='origin at lower left'  
 		 ); 
 		*/
-		cv::putText(img, e, origin, font_face, font_scale, cv::Scalar(0, 255, 255), thickness, 8, 0);
+
+		cv::putText(img, e, origin, font_face, font_scale, cv::Scalar(0, 0, 255), text_thickness, 8, 0);
+	    
+	}
+
+	if( transparent)
+	{
+		float beta = ( 1.0 - alpha );
+		cv::addWeighted(dst, alpha, img, beta, 0 ,dst);
+		img = dst;
+	}
+}
+
+void frameTextCh(cv::Mat&img, vector<string>&text, const bool&filled = true, const bool&transparent = true, const float&alpha = 0.3)
+{
+	//设置绘制文本的相关参数
+	//std::string text = "Hello World!";
+	int fontHeight = 30;
+	int baseline;
+	//获取文本框的长宽
+	int y = 0;
+	cv::Point origin; 
+
+	Ptr<freetype::FreeType2> ft2;
+	ft2 = freetype::createFreeType2();
+	//ft2->loadFontData( "xjlFont.ttf", 0 );
+	ft2->loadFontData( "simsun.ttc", 0 );
+	
+	Mat dst;
+	if(transparent)
+	{
+		img.copyTo(dst);
+	}
+
+	for(auto&e:text)
+	{
+		int text_thickness =2;
+		cv::Size text_size = ft2->getTextSize(e, fontHeight, text_thickness, &baseline);
+		
+		cout<<"cv::Size height:"<<text_size.height<<endl;
+		cout<<"cv::Size width:"<<text_size.width<<endl;
+								 
+		//将文本框居中绘制
+		origin.x = 0;
+		if( y == 0)
+			origin.y = text_size.height+10;
+	    origin.y += y;
+		y += origin.y;
+		cout<<"y:"<<y<<endl;
  
 		Rect select;//声明矩形
 		select.x = origin.x;//左上角坐标
 		select.y = origin.y - text_size.height - 5;
 		select.width = text_size.width;
 		select.height = text_size.height+12;
-		 
-		rectangle(img,select,Scalar(0,0,255),3,8,0);//用矩形画矩形窗
+		
+		int thickness = 3;
+    	if(filled)
+    		thickness = FILLED;
+		Scalar s = getFilledColor();
+		//rectangle(img,select, s,thickness,8,0);//用矩形画矩形窗
+		rectangle(img,select, s,3,8,0);//用矩形画矩形窗
+		
+		/*
+    	void cv::putText(  
+		 cv::Mat& img, // 待绘制的图像  
+		 const string& text, // 待绘制的文字  
+		 cv::Point origin, // 文本框的左下角  
+		 int fontFace, // 字体 (如cv::FONT_HERSHEY_PLAIN)  
+		 double fontScale, // 尺寸因子，值越大文字越大  
+		 cv::Scalar color, // 线条的颜色（RGB）  
+		 int thickness = 1, // 线条宽度  
+		 int lineType = 8, // 线型（4邻域或8邻域，默认8邻域）  
+		 bool bottomLeftOrigin = false // true='origin at lower left'  
+		 ); 
+		*/
+		ft2->putText(img, e, origin, fontHeight, cv::Scalar(0, 0, 255), text_thickness, 8, 0);
+	}
+
+	if( transparent)
+	{
+		float beta = ( 1.0 - alpha );
+		cv::addWeighted(dst, alpha, img, beta, 0 ,dst);
+		img = dst;
 	}
 }
-
 void frameText(cv::Mat&img,const string&text)
 {
 	//设置绘制文本的相关参数
@@ -145,11 +269,12 @@ int main(int argc,char *argv[])
     cv::circle(img,cv::Point(100,100),50,cv::Scalar(0,255,0),2);
 	
 	string text = "Hello World";
-	//frameText(img, text);
 	vector<string>vecText;
 	vecText.push_back(text);
+	text = "Hello World 123 ";
 	vecText.push_back(text);
 	frameText(img, vecText);
+	//frameTextCh(img, vecText);
     
 	cv::namedWindow("test");
     cv::imshow("test",img);
@@ -173,10 +298,10 @@ int main(int argc,char *argv[])
 	contours.push_back(contour);
 
 	//填充区域之前，首先采用polylines()函数，可以使填充的区域边缘更光滑
-	cv::polylines(src, contours, true, cv::Scalar(255, 255, 255), 2, cv::LINE_AA);//第2个参数可以采用contour或者contours，均可
-	cv::fillPoly(src, contours, cv::Scalar(255, 255, 255));//fillPoly函数的第二个参数是二维数组！！
+	//cv::polylines(src, contours, true, cv::Scalar(255, 255, 255), 2, cv::LINE_AA);//第2个参数可以采用contour或者contours，均可
+	//cv::fillPoly(src, contours, cv::Scalar(255, 255, 255));//fillPoly函数的第二个参数是二维数组！！
 
-	cv::imshow("原图", src);
+	//cv::imshow("原图", src);
 	cv::waitKey(0);
     
 	return 0;
